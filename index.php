b8d86b18e8dbfa478001981677fec81b782a5091
Carlos Cuartas <carloscuartasm@gmail.com>
2020-05-27 12:28:41 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+  session_start();
+  require_once 'header.php';
+
+  echo "<div class='center'>Welcome to Social Net,";
+
+  if ($loggedin) echo " $user, you are logged in";
+  else           echo ' please sign up or log in';
+
+  echo <<<_END
+      </div><br>
+    </div>
+    <div data-role="footer">
+      <h4>Web App developed by <i><a href='https://carloscuartas.com'
+      target='_blank'>Carlos Cuartas Marin</a></i></h4>
+    </div>
+  </body>
+</html>
+_END;
+?>
